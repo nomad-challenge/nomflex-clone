c3fa48c23847b77e3e84a3dd70f283f7a6aaf501
antnf3 <antnf3@gmail.com>
2023-07-15 11:27:57 +0900

    header circle move animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,79 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import Tv from "./routes/Tv";
+import Search from "./routes/Search";
+import Header from "./components/Header";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyledReset = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Lunasima:wght@400;700&display=swap');
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+/* HTML5 hidden-attribute fix for newer browsers */
+*[hidden] {
+    display: none;
+}
+body {
+  line-height: 1;
+  box-sizing: border-box;
+  font-family: 'Lunasima', sans-serif;
+}
+menu, ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+a {
+  text-decoration: none;
+}
+`;
 function App() {
-  return <h1>nomflix-clone</h1>;
+  return (
+    <>
+      <GlobalStyledReset />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/tv" element={<Tv />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
