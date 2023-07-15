87540ae3df3ee3ebea198d6fa58a9ebc64213647
antnf3 <antnf3@gmail.com>
2023-07-15 12:34:53 +0900

    globalcss 위치변경, queryClient 설정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import {
   useMotionValueEvent,
   useScroll,
 } from "framer-motion";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link, useMatch } from "react-router-dom";
 import { styled } from "styled-components";
 
